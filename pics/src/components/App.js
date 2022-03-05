f6116fb1b72fb3f0e82de8a2eddb248237313d87
Claudia Ziemba <claudia.ziemba@urbetrack.com>
2022-03-04 22:24:50 -0300

    adding request to unsplash

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
 
-  onSearchSubmit(term){
-    console.log(term);
+  async onSearchSubmit(term){
+    const result = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term},
+      headers: {Authorization: "Client-ID 7K53AgnGiMcjIlo_jnlnZST6cxKBcX-0P63QIRZ3KfA"}
+    });
+
+    
   }
 
   render(){
